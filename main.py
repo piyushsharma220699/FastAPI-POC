6d1a99d87faa99c8872f11fa90600f587ab30c8f
Piyush Sharma <piyush.sharma@kotak.com>
2023-07-28 16:16:52 +0530

    Created GET and POST operations on database

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from models import Customer
+from database import db
 
 app = FastAPI()
 
-@app.get("/")
-def demo():
-    return {"Hello": "World"}
+@app.get("/api/v1/allcustomers")
+def fetch_all_customers():
+    return db
+
+@app.get("/api/v1/customer/{customer_id}")
+def get_customer_detail(customer_id: int):
+    for customer_detail in db:
+        if customer_detail.id == customer_id:
+            return customer_detail
+
+@app.post("/api/v1/customer")
+def add_customer_detail(customer: Customer):
+    db.append(customer)
+    return {"id": customer.id}
--- models.py
+++ models.py
@@ -1,23 +1,25 @@
 from pydantic import BaseModel
 from enum import Enum
 from typing import Optional
+from uuid import UUID, uuid4
 
 class Gender(str, Enum):
     male = "Male"
     female = "Female"
 
-class Address(BaseModel):
-    street: Optional[str]
-    city: Optional[str]
-    state: Optional[str]
-    country: str
+# class Address(BaseModel):
+#     street: Optional[str]
+#     city: Optional[str]
+#     state: Optional[str]
+#     country: str
 
 class Customer(BaseModel):
+    id: Optional[UUID] = uuid4()
     first_name: str
     last_name: str
     gender: Gender
     email: str
-    address: Optional[Address]
+    # address: Optional[Address]
     account_number: int
     balance: int
     is_premium: bool
